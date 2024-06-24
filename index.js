3e8518cc7f3368fcd5b63aeea81cc25f05b9ea2a
Bethuel Khisa <sunguribethuel@gmail.com>
2024-06-24 14:04:57 +0300

    Adds all the variables

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+const currentUser = "Bethuel";
+
+// Using backticks to represent strings an using string interpolation.
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Converting the currentUser name to upperCase so as to reference it.
+const upperCaseCurrentUser = currentUser.toUpperCase();
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+
+// Another variable
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+const shortGreeting = `Welcome, ${firstInitial}!`;
